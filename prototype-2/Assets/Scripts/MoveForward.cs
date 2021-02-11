6bf844d4fa1b77d83bd9b5d6d05be862375b0790
Emma <emmaferris2242@gmail.com>
2021-02-10 22:35:45 -0700

    script for player and for bone started

---
+++ prototype-2/Assets/Scripts/MoveForward.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+    private float speed = 20.0f; 
+
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        // tells the bone to move forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
---
+++ prototype-2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float hInput;
+    public float speed = 20.0f;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        //tell it where to get movement info
+        hInput = Input.GetAxis("Horizontal");
+
+        // sets constraints on the right side
+        if(transform.position.x > 15)
+        {
+            transform.position = new Vector3(15, transform.position.y, transform.position.z);
+        }
+
+        //sets constraints on the left side
+        if(transform.position.x < -15)
+        {
+            transform.position = new Vector3(-15, transform.position.y, transform.position.z);
+        }
+
+        //tells it how to move, left makes it inverted but i like it
+        transform.Translate(Vector3.left * hInput * Time.deltaTime * speed);
+    }   
+}
