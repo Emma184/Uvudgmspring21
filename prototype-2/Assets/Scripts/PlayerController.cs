22cab6982186d81fe7574a5c4de4a3192b2fba7a
Emma <emmaferris2242@gmail.com>
2021-02-14 20:16:38 -0700

    tried to finish it but scripting problems set it back

--- prototype-2/Assets/Scripts/PlayerController.cs
+++ prototype-2/Assets/Scripts/PlayerController.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.EventSystems.EventTrigger;
 
 public class PlayerController : MonoBehaviour
 {
     public float hInput;
     private float speed = 20.0f;
     public float xRange = 24;
-    public GameObject projectilePreFab;
+    public GameObject projectilePrefab;
 
     // Update is called once per frame
     void Update()
@@ -15,14 +16,15 @@ public class PlayerController : MonoBehaviour
         //tell it where to get movement info
         hInput = Input.GetAxis("Horizontal");
 
+
         // sets constraints on the right side
-        if(transform.position.x > xRange)
+        if (transform.position.x > xRange)
         {
             transform.position = new Vector3(24, transform.position.y, transform.position.z);
         }
 
         //sets constraints on the left side
-        if(transform.position.x < -xRange)
+        if (transform.position.x < -xRange)
         {
             transform.position = new Vector3(-24, transform.position.y, transform.position.z);
         }
@@ -30,9 +32,9 @@ public class PlayerController : MonoBehaviour
         //tells it how to move, left makes the controlls inverted but i like it
         transform.Translate(Vector3.left * hInput * Time.deltaTime * speed);
     }   
-    // supposed to help launch it but it not working yet
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-        //Launches the projectile
-        Instantiate(projectilePreFab, transfom.position, projectilePreFab.transform.rotation);
+    
+        
+       
+       
+        
 }
