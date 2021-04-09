5857d533c4dc73ffbb44f5641ce8b29de128698c
Emma <emmaferris2242@gmail.com>
2021-04-09 16:42:58 -0600

    got the player moving and imported prototype3

--- Personal Project/Assets/Scripts/PlayerController.cs
+++ Personal Project/Assets/Scripts/PlayerController.cs
@@ -4,16 +4,51 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
-    public bool isClear = true;
+    //controller for movement  and speed
+   
+    public float movementSpeed = 5;
+   
+   
+    
+ 
+
 
     // Start is called before the first frame update
     void Start()
-    { transform.Translate,left * speed;  }
+    {
+
+    }
+
+
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        //moves player forward
+        if (Input.GetKey("w"))
+        {
+            
+            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
+        }
+        // moves player backwards
+        else if (Input.GetKey("s"))
+        {
+            
+            transform.position -= transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
+        }
+        // moves player left
+        if(Input.GetKey("a") && !Input.GetKey("d") )
+        {
+            transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
+        }
+        // moves player right
+        else if(Input.GetKey("d") && !Input.GetKey("a"))
+        {
+            transform.position -= transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
+
+        }
+    }
+   void Update()
+    {
     }
 }
