ea0b2c6386781856691e0e11d40fa288c40d8f00
Emma <emmaferris2242@gmail.com>
2021-04-14 17:16:49 -0600

    restarted and got to the spawn manager stuff

---
+++ protype-4 the retry/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{ 
+    //speed and rigidbody 
+        private Rigidbody playerRb;
+        public float speed;
+    //camera focus
+        private GameObject focalPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("focalPoint");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * speed * Time.deltaTime);
+
+
+    }
+       
+        
+
+    
+    
+}
