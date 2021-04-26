0aedbe6184178676368f50db585b5a998ea0dce7
Emma <emmaferris2242@gmail.com>
2021-04-26 17:26:16 -0600

    finished basic set up and added candy into it.

--- Personal Project/Assets/Scripts/PlayerControllerX.cs
+++ Personal Project/Assets/Scripts/PlayerControllerX.cs
@@ -2,53 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerControllerX : MonoBehaviour
 {
-    //controller for movement  and speed
    
+    public bool isClear = true;
     public float movementSpeed = 5;
-   
-   
-    
- 
+    private Rigidbody playerRb;
+    public GameObject Player;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-
+        playerRb = GetComponent<Rigidbody>();
     }
 
-
-
     // Update is called once per frame
-    private void FixedUpdate()
-    {
-        //moves player forward
+    void Update()
+    {//moves player forward 
         if (Input.GetKey("w"))
         {
-            
-            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
-        }
-        // moves player backwards
+
+            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed; }
+        // moves player backwards 
         else if (Input.GetKey("s"))
         {
-            
             transform.position -= transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
         }
         // moves player left
-        if(Input.GetKey("a") && !Input.GetKey("d") )
+        if (Input.GetKey("a") && !Input.GetKey("d"))
         {
             transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
         }
         // moves player right
-        else if(Input.GetKey("d") && !Input.GetKey("a"))
+        else if (Input.GetKey("d") && !Input.GetKey("a"))
         {
             transform.position -= transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
-
         }
-    }
-   void Update()
-    {
+        
+        }
+    private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Candy"))
+            {
+                Destroy(other.gameObject);
+                Debug.Log("Candy Collected");
+            }
     }
 }
