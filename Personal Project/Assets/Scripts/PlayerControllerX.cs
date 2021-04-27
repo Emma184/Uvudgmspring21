0e16f860897fe01f2ebdecf902342bee7a5a424c
Emma <emmaferris2242@gmail.com>
2021-04-26 18:41:27 -0600

    added enemy and candy basic mechanic, still don't know the hedge stuff

--- Personal Project/Assets/Scripts/PlayerControllerX.cs
+++ Personal Project/Assets/Scripts/PlayerControllerX.cs
@@ -15,39 +15,66 @@ public class PlayerControllerX : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //instantiates the player's rigidbody
         playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
-    {//moves player forward 
+    {
+        //move player throu wasd keys
         if (Input.GetKey("w"))
         {
-
-            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed; }
-        // moves player backwards 
+            //moves player forward 
+            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
+        }
+        
         else if (Input.GetKey("s"))
         {
+            // moves player backwards 
             transform.position -= transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
         }
-        // moves player left
+       
         if (Input.GetKey("a") && !Input.GetKey("d"))
-        {
+        { 
+            // moves player left
             transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
         }
-        // moves player right
+        
         else if (Input.GetKey("d") && !Input.GetKey("a"))
         {
+            // moves player right
             transform.position -= transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
         }
         
-        }
+    }
     private void OnTriggerEnter(Collider other)
-        {
-            if (other.CompareTag("Candy"))
+    {
+        // tells you if you collected the candy
+            if (other.CompareTag("candy"))
             {
                 Destroy(other.gameObject);
                 Debug.Log("Candy Collected");
             }
+            //for when the player hits the obstacles so they don't go through them
+        if (other.CompareTag("obstacles"))
+        {
+            Rigidbody obstacleRigidbody = other.gameObject.GetComponent<Rigidbody>();
+            Vector3 boopPlayer = transform.position + other.gameObject.transform.position;
+                Debug.Log("boop");
+        }
+        // for when the player gets close to an enemy
+            if (other.CompareTag("enimies"))
+            {
+            //got idea from challenge 4
+               ResetPlayerPosition();
+                Debug.Log("Raccoon attack");
+            }
+    }
+   //got inspiration from challenge 4
+    private void ResetPlayerPosition()
+    {
+        Player.transform.position = new Vector3(1,1,18);
+
     }
 }
