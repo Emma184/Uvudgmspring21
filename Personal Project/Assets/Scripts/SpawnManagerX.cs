e095f34abdb242e66464e17b719d7f1a9f66a2c5
Emma <emmaferris2242@gmail.com>
2021-05-03 14:54:15 -0600

    added models in

--- Personal Project/Assets/Scripts/SpawnManagerX.cs
+++ Personal Project/Assets/Scripts/SpawnManagerX.cs
@@ -9,49 +9,49 @@ public class SpawnManagerX : MonoBehaviour
     public GameObject enemyPrefab;
     private float spawnRangex = 30;
     private float spawnRangez = 30;
-    private PlayerControllerX playerControllerScript;
-    private float spawnDelay = 2; 
-    private float spawnInterval = 1.5f;
+    private PlayerControllerX playerControllerScript; 
+    private float spawnInterval = 5.0f;
     private Vector3 spawnPos = new Vector3(19, 13);
+    public int objectCount;
+    private int objectCap =9;
+    
+
     // Start is called before the first frame update 
     void Start()
     {
-        InvokeRepeating("SpawnObject", spawnDelay, spawnInterval);
+        InvokeRepeating("SpawnObject", 0, spawnInterval);
         playerControllerScript = GameObject.Find("Player").GetComponent<PlayerControllerX>();
-
-    }
-    private void Update()
-    {
-         // Spawn obstacles 
-    // If game is still active, spawn new object 
-        if (playerControllerScript.gameOver == false)
-        { // Set random spawn location and random object index
-       
-            Instantiate(candyPrefab, GenerateRandomSpawns(), candyPrefab.transform.rotation);
-            Instantiate(enemyPrefab, GenerateRandomSpawns(), candyPrefab.transform.rotation);
-
-        }
-        if ()
-        {
-
-        }
         
     }
+    private void SpawnObject()
+    {      
+            // Spawn obstacles 
+            // If game is still active, spawn new object 
+            if (playerControllerScript.gameOver == false && objectCount < 5)
+            { // Set random spawn location and random object index
+
+                Instantiate(candyPrefab, GenerateRandomSpawns(), candyPrefab.transform.rotation);
+        
+                Instantiate(enemyPrefab, GenerateRandomSpawns(), candyPrefab.transform.rotation);
+            objectCount++;
+            if (objectCount >= objectCap)
+            {
+                CancelInvoke("Spawn");
+            }
+            }
+
+
+        
+       
+    }
+
+
     private Vector3  GenerateRandomSpawns()
     {
         float spawnPosX = Random.Range(-spawnRangex, spawnRangex);
         float spawnPosZ = Random.Range(-spawnRangez, spawnRangez);
         Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
-         return ;
+         return randomPos;
     }
-    void DeleteEnemies()
-    {
-
-        IEnumerator PowerupCountdownRoutine()
-        {
-            yield return new WaitForSeconds(20);
-            DestroyObject(enemyPrefab);
-            
-        }
-    }
+    
 }
